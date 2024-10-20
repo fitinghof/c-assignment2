d928f3355222740114cf57c33e6cd605eef30727
Simon Nilsson <simon.f.nilsson@me.com>
2024-10-20 13:35:12 +0200

    progress?

--- dynamic_array.c
+++ dynamic_array.c
@@ -1,25 +1,27 @@
 #define _GNU_SOURCE
 #include "dynamic_array.h"
-#include <pthread.h>
+
 #include <pthread.h>
 
 /*
     Merge memory manager and dynamic array?
 
-    Should probably implement locks for each memoryblock or array index or whatever
-    and have thread's trying to loop over those be locked until the thread infron is finished
-    threads should ofcourse also never be allowed to stop on the same index when looping
-    might be mega complicated to implement but it's a consideration. It might allow for
-    function to have a "soft" write lock, allowing changes to be made in that index, like perhaps
-    setting a memoryblock as used
+    Should probably implement locks for each memoryblock or array index or
+   whatever and have thread's trying to loop over those be locked until the
+   thread infron is finished threads should ofcourse also never be allowed to
+   stop on the same index when looping might be mega complicated to implement
+   but it's a consideration. It might allow for function to have a "soft" write
+   lock, allowing changes to be made in that index, like perhaps setting a
+   memoryblock as used
 
-    A split function would probably be good, this function would require complete array write/read protection
-    but could probably be made fairly fast.
+    A split function would probably be good, this function would require
+   complete array write/read protection but could probably be made fairly fast.
 
     Dynamic array? :)
         + low memory overhead
         + fast to iterate over
-        - splitting would likely require locking the entire thing ( If I even go the splitting route )
+        - splitting would likely require locking the entire thing ( If I even go
+   the splitting route )
 
 
     linked list? :(
@@ -34,26 +36,18 @@
 
 pthread_rwlock_t rwlock;
 
-void shove_left_(dynamic_array_head* head, size_t index) {
+void shove_left_(dynamic_array_head* head, size_t index) {}
 
-}
+void shove_right_(dynamic_array_head* head, size_t index) {}
 
-void shove_right_(dynamic_array_head* head, size_t index) {
+void resize_(dynamic_array_head* head, size_t new_size) {}
 
-}
-
-void resize_(dynamic_array_head* head, size_t new_size) {
-
-}
-
-void add_last_(dynamic_array_head* head, memory_block block){
-    if(head->current_size == head->total_size)
+void add_last_(dynamic_array_head* head, memory_block block) {
+    if (head->current_size == head->total_size)
         resize_(head, head->current_size * 2);
     head->array[head->current_size++] = block;
     return;
 }
-
-
 
 void DA_init(dynamic_array_head* head, size_t init_size) {
     head->array = calloc(init_size, sizeof(*(head->array)));
@@ -62,15 +56,25 @@ void DA_init(dynamic_array_head* head, size_t init_size) {
     pthread_rwlock_init(&rwlock, NULL);
 }
 
-
-void *DA_add_mem_block(dynamic_array_head* head, size_t size) {
+void* DA_add_mem_block(dynamic_array_head* head, size_t size) {
     pthread_rwlock_wrlock(&rwlock);
-    memory_block *best_fit = NULL;
-    for(size_t i = 0; i < head->current_size; i++){
-        if(head->array[i].free && head->array[i].size >= size){
+    memory_block *walker = DA_get_first__nolock__(head);
+    uint32_t pointer_ofset = 0;
+    while(walker != NULL){
+        while(!walker->free) {
+            walker = DA_get_next__nolock__(head, walker);
+        }
+        uint32_t total_free = 0;
+        memory_block *free_block_start = walker;
+        while(walker->free) {
+            total_free += walker->size;
+            walker = DA_get_next__nolock__(head, walker);
+        }
+        if(total_free >= size){
 
         }
     }
+
     pthread_rwlock_unlock(&rwlock);
     return NULL;
 }
@@ -79,20 +83,24 @@ void DA_remove(dynamic_array_head* head, memory_block* block) {
 
 }
 
-memory_block* DA_find(dynamic_array_head* head, void* start) {
-
+memory_block* DA_get_next__nolock__(dynamic_array_head* head, memory_block* block) {
+    block++;
+    while (block < head->array + head->total_size) {
+        if(block->exists) {
+            pthread_rwlock_unlock(&rwlock);
+            return block;
+        }
+        block++;
+    }
+    return NULL;
 }
 
-memory_block* DA_get_next(dynamic_array_head* head, memory_block* block) {
-
+memory_block* DA_get_first__nolock__(dynamic_array_head* head) {
+    if (head->array[0].exists) return head->array;
+    return DA_get_next__nolock__(head, head->array);
 }
 
-memory_block *DA_get_first(dynamic_array_head *head){
-
-}
-
-
-void DA_deinit(dynamic_array_head *head){
+void DA_deinit(dynamic_array_head* head) {
     free(head->array);
     pthread_rwlock_destroy(&rwlock);
 }
--- dynamic_array.h
+++ dynamic_array.h
@@ -11,9 +11,9 @@
 #include <stdlib.h>
 
 typedef struct {
-    void *start;
     uint32_t size;
     bool free;
+    bool exists;
 } memory_block;
 
 typedef struct dynamic_array_head {
@@ -24,7 +24,7 @@ typedef struct dynamic_array_head {
 
 void DA_init(dynamic_array_head *head, size_t init_size);
 
-void *DA_add_mem_block(dynamic_array_head *head, memory_block block);
+void *DA_add_mem_block(dynamic_array_head *head, size_t size);
 
 void DA_remove(dynamic_array_head *head, memory_block *block);
 
@@ -32,7 +32,7 @@ memory_block *DA_find(dynamic_array_head *head, void *start);
 
 memory_block *DA_get_next(dynamic_array_head *head, memory_block *block);
 
-memory_block *DA_get_first(dynamic_array_head *head);
+memory_block *DA_get_first__nolock__(dynamic_array_head *head);
 
 void DA_deinit(dynamic_array_head *head);
 
