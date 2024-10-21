b469762eb7874d5635101cba349a0d8c18a7fdff
Simon Nilsson <simon.f.nilsson@me.com>
2024-10-22 00:34:32 +0200

    Multi threading

--- linked_list.c
+++ linked_list.c
@@ -1,11 +1,18 @@
-#define __USE_UNIX98
+#define _GNU_SOURCE
 #include "linked_list.h"
+
+pthread_rwlock_t lock;
 
 /// @brief Initializes the list
 /// @param head list head
 void list_init(Node** head, size_t size) {
     mem_init(size);
     *head = NULL;
+    int init_result = pthread_rwlock_init(&lock, NULL);
+    if (init_result != 0) {
+        perror("pthread_rwlock_init failed");
+        exit(EXIT_FAILURE);
+    }
 }
 
 /// @brief inserts last in linked list
@@ -13,11 +20,16 @@ void list_init(Node** head, size_t size) {
 /// @param data data for the new node
 void list_insert(Node** head, uint16_t data) {
     Node* new_node = mem_alloc(sizeof(Node));
-    if (!new_node) return;
+    pthread_rwlock_wrlock(&lock);
+    if (!new_node) {
+        pthread_rwlock_unlock(&lock);
+        return;
+    }
     new_node->data = data;
     new_node->next = NULL;
     if (*head == NULL) {
         *head = new_node;
+        pthread_rwlock_unlock(&lock);
         return;
     }
     Node* walker = *head;
@@ -25,6 +37,8 @@ void list_insert(Node** head, uint16_t data) {
         walker = walker->next;
     }
     walker->next = new_node;
+    pthread_rwlock_unlock(&lock);
+    return;
 }
 
 /// @brief Inserts a node after prev_node
@@ -34,9 +48,11 @@ void list_insert_after(Node* prev_node, uint16_t data) {
     if (prev_node == NULL) return;
     Node* new_node = mem_alloc(sizeof(Node));
     if (!new_node) return;
+    pthread_rwlock_wrlock(&lock);
     new_node->next = prev_node->next;
     new_node->data = data;
     prev_node->next = new_node;
+    pthread_rwlock_unlock(&lock);
 }
 
 /// @brief inserts before a node
@@ -44,48 +60,70 @@ void list_insert_after(Node* prev_node, uint16_t data) {
 /// @param next_node node that will be after new node
 /// @param data data for the new node
 void list_insert_before(Node** head, Node* next_node, uint16_t data) {
-    if (*head == NULL) return;  // ERROR
-    Node* walker = *head;
-    if (next_node == *head) {
-        Node* new_node = mem_alloc(sizeof(Node));
-        if (!new_node) return;
-
-        new_node->data = data;
-        new_node->next = *head;
-        *head = new_node;
+    pthread_rwlock_wrlock(&lock);
+    if (*head == NULL) {
+        return;  // ERROR
+        pthread_rwlock_unlock(&lock);
+    }
+    Node* new_node = mem_alloc(sizeof(Node));
+    if (!new_node) {
+        pthread_rwlock_unlock(&lock);
         return;
     }
 
+    if (next_node == *head) {
+        new_node->data = data;
+        new_node->next = *head;
+        *head = new_node;
+        pthread_rwlock_unlock(&lock);
+        return;
+    }
+
+    Node* walker = *head;
     while (walker->next != next_node && walker->next != NULL) {
         walker = walker->next;
     }
-    if (walker->next == NULL) return;  // ERRROR
-    Node* new_node = mem_alloc(sizeof(Node));
-    if (!new_node) return;
+    if (walker->next == NULL) {
+        pthread_rwlock_unlock(&lock);
+        mem_free(new_node);
+        return;  // ERRROR
+    }
     walker->next = new_node;
     walker->next->next = next_node;
     walker->next->data = data;
+    pthread_rwlock_unlock(&lock);
+    return;
 }
 
 /// @brief deletes the Node with data
 /// @param head list head
 /// @param data
 void list_delete(Node** head, uint16_t data) {
-    if (*head == NULL) return;
+    pthread_rwlock_wrlock(&lock);
+    if (*head == NULL){
+        pthread_rwlock_unlock(&lock);
+        return;
+    }
     if ((*head)->data == data) {
         Node* temp = *head;
         *head = (*head)->next;
         mem_free(temp);
+        pthread_rwlock_unlock(&lock);
         return;
     }
     Node* walker = *head;
     while (walker->next != NULL && walker->next->data != data) {
         walker = walker->next;
     }
-    if (walker->next == NULL) return;
+    if (walker->next == NULL) {
+        pthread_rwlock_unlock(&lock);
+        return;
+    }
     Node* temp = walker->next;
     walker->next = temp->next;
     mem_free(temp);
+    pthread_rwlock_unlock(&lock);
+    return;
 }
 
 /// @brief return the pointer to node with data or NULL if not found
@@ -93,11 +131,16 @@ void list_delete(Node** head, uint16_t data) {
 /// @param data value to search for
 /// @return Node* or NULL if node not found
 Node* list_search(Node** head, uint16_t data) {
+    pthread_rwlock_rdlock(&lock);
     Node* walker = *head;
     while (walker != NULL) {
-        if (walker->data == data) return walker;
+        if (walker->data == data) {
+            pthread_rwlock_unlock(&lock);
+            return walker;
+        }
         walker = walker->next;
     }
+    pthread_rwlock_unlock(&lock);
     return NULL;
 }
 
@@ -110,6 +153,7 @@ void list_display(Node** head) { list_display_range(head, NULL, NULL); }
 /// @param start_node first node to display
 /// @param end_node last node to display
 void list_display_range(Node** head, Node* start_node, Node* end_node) {
+    pthread_rwlock_rdlock(&lock);
     if (end_node) end_node = end_node->next;
     if (!start_node) start_node = *head;
     printf("[");
@@ -119,19 +163,21 @@ void list_display_range(Node** head, Node* start_node, Node* end_node) {
         if (start_node && start_node != end_node) printf(", ");
     }
     printf("]");
+    pthread_rwlock_unlock(&lock);
 }
 
 /// @brief returns the number of nodes
 /// @param head list head
 /// @return int
 int list_count_nodes(Node** head) {
-    if (*head == NULL) return 0;
+    pthread_rwlock_rdlock(&lock);
     Node* walker = *head;
     int counter = 0;
     while (walker != NULL) {
         counter++;
         walker = walker->next;
     }
+    pthread_rwlock_unlock(&lock);
     return counter;
 }
 
@@ -146,4 +192,5 @@ void list_cleanup(Node** head) {
     }
     *head = NULL;
     mem_deinit();
+    pthread_rwlock_destroy(&lock);
 }
