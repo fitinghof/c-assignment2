773b787b5a26e4b56a737a2e8f2a152629540cdc
Simon Nilsson <simon.f.nilsson@me.com>
2024-10-24 03:22:26 +0200

    added multithreading for real

--- linked_list.c
+++ linked_list.c
@@ -1,14 +1,14 @@
 #define _GNU_SOURCE
 #include "linked_list.h"
 
-pthread_rwlock_t lock;
+pthread_rwlock_t head_lock;
 
 /// @brief Initializes the list
 /// @param head list head
 void list_init(Node** head, size_t size) {
     mem_init(size);
     *head = NULL;
-    int init_result = pthread_rwlock_init(&lock, NULL);
+    int init_result = pthread_rwlock_init(&head_lock, NULL);
     if (init_result != 0) {
         perror("pthread_rwlock_init failed");
         exit(EXIT_FAILURE);
@@ -20,24 +20,27 @@ void list_init(Node** head, size_t size) {
 /// @param data data for the new node
 void list_insert(Node** head, uint16_t data) {
     Node* new_node = mem_alloc(sizeof(Node));
-    pthread_rwlock_wrlock(&lock);
-    if (!new_node) {
-        pthread_rwlock_unlock(&lock);
-        return;
-    }
+    pthread_mutex_init(&new_node->lock, NULL);
+    if (!new_node) return;
     new_node->data = data;
     new_node->next = NULL;
+    pthread_rwlock_wrlock(&head_lock);
     if (*head == NULL) {
         *head = new_node;
-        pthread_rwlock_unlock(&lock);
+        pthread_rwlock_unlock(&head_lock);
         return;
     }
     Node* walker = *head;
+    pthread_mutex_lock(&walker->lock);
+    pthread_rwlock_unlock(&head_lock);
+
     while (walker->next) {
+        pthread_mutex_unlock(&walker->lock);
+        pthread_mutex_lock(&walker->next->lock);
         walker = walker->next;
     }
     walker->next = new_node;
-    pthread_rwlock_unlock(&lock);
+    pthread_mutex_unlock(&walker->lock);
     return;
 }
 
@@ -47,12 +50,14 @@ void list_insert(Node** head, uint16_t data) {
 void list_insert_after(Node* prev_node, uint16_t data) {
     if (prev_node == NULL) return;
     Node* new_node = mem_alloc(sizeof(Node));
+    pthread_mutex_init(&new_node->lock, NULL);
+
     if (!new_node) return;
-    pthread_rwlock_wrlock(&lock);
-    new_node->next = prev_node->next;
     new_node->data = data;
+    pthread_mutex_lock(&prev_node->lock);
+    new_node->next = prev_node->next;
     prev_node->next = new_node;
-    pthread_rwlock_unlock(&lock);
+    pthread_mutex_unlock(&prev_node->lock);
 }
 
 /// @brief inserts before a node
@@ -60,38 +65,50 @@ void list_insert_after(Node* prev_node, uint16_t data) {
 /// @param next_node node that will be after new node
 /// @param data data for the new node
 void list_insert_before(Node** head, Node* next_node, uint16_t data) {
-    pthread_rwlock_wrlock(&lock);
+    pthread_rwlock_wrlock(&head_lock);
+
     if (*head == NULL) {
+        pthread_rwlock_unlock(&head_lock);
         return;  // ERROR
-        pthread_rwlock_unlock(&lock);
     }
+    pthread_mutex_lock(&(*head)->lock);
+    pthread_rwlock_unlock(&head_lock);
+
     Node* new_node = mem_alloc(sizeof(Node));
     if (!new_node) {
-        pthread_rwlock_unlock(&lock);
+        pthread_mutex_unlock(&(*head)->lock);
         return;
     }
+    pthread_mutex_init(&new_node->lock, NULL);
+    pthread_mutex_lock(&new_node->lock);
 
     if (next_node == *head) {
         new_node->data = data;
         new_node->next = *head;
         *head = new_node;
-        pthread_rwlock_unlock(&lock);
+        pthread_mutex_unlock(&new_node->lock);
+        pthread_mutex_unlock(&new_node->next->lock);
         return;
     }
 
     Node* walker = *head;
     while (walker->next != next_node && walker->next != NULL) {
+        pthread_mutex_lock(&walker->next->lock);
+        pthread_mutex_t *old_lock = &walker->lock;
         walker = walker->next;
+        pthread_mutex_unlock(old_lock);
     }
     if (walker->next == NULL) {
-        pthread_rwlock_unlock(&lock);
+        pthread_mutex_unlock(&walker->lock);
+        pthread_mutex_unlock(&new_node->lock);
         mem_free(new_node);
         return;  // ERRROR
     }
     walker->next = new_node;
     walker->next->next = next_node;
     walker->next->data = data;
-    pthread_rwlock_unlock(&lock);
+    pthread_mutex_unlock(&new_node->lock);
+    pthread_mutex_unlock(&walker->lock);
     return;
 }
 
@@ -99,30 +116,42 @@ void list_insert_before(Node** head, Node* next_node, uint16_t data) {
 /// @param head list head
 /// @param data
 void list_delete(Node** head, uint16_t data) {
-    pthread_rwlock_wrlock(&lock);
+    pthread_rwlock_wrlock(&head_lock);
     if (*head == NULL){
-        pthread_rwlock_unlock(&lock);
+        pthread_rwlock_unlock(&head_lock);
         return;
     }
+
     if ((*head)->data == data) {
+        pthread_mutex_lock(&(*head)->lock);
         Node* temp = *head;
         *head = (*head)->next;
+        pthread_mutex_unlock(&temp->lock);
         mem_free(temp);
-        pthread_rwlock_unlock(&lock);
+        pthread_rwlock_unlock(&head_lock);
         return;
     }
     Node* walker = *head;
+    pthread_mutex_lock(&walker->lock);
+    pthread_rwlock_unlock(&head_lock);
+
     while (walker->next != NULL && walker->next->data != data) {
+        pthread_mutex_lock(&walker->next->lock);
+        pthread_mutex_t *old_lock = &walker->lock;
         walker = walker->next;
+        pthread_mutex_unlock(old_lock);
     }
     if (walker->next == NULL) {
-        pthread_rwlock_unlock(&lock);
+        pthread_mutex_unlock(&walker->lock);
         return;
     }
+
     Node* temp = walker->next;
+    pthread_mutex_lock(&temp->lock);
     walker->next = temp->next;
+    pthread_mutex_unlock(&temp->lock);
+    pthread_mutex_unlock(&walker->lock);
     mem_free(temp);
-    pthread_rwlock_unlock(&lock);
     return;
 }
 
@@ -131,16 +160,16 @@ void list_delete(Node** head, uint16_t data) {
 /// @param data value to search for
 /// @return Node* or NULL if node not found
 Node* list_search(Node** head, uint16_t data) {
-    pthread_rwlock_rdlock(&lock);
+    pthread_rwlock_rdlock(&head_lock);
     Node* walker = *head;
     while (walker != NULL) {
         if (walker->data == data) {
-            pthread_rwlock_unlock(&lock);
+            pthread_rwlock_unlock(&head_lock);
             return walker;
         }
         walker = walker->next;
     }
-    pthread_rwlock_unlock(&lock);
+    pthread_rwlock_unlock(&head_lock);
     return NULL;
 }
 
@@ -153,7 +182,7 @@ void list_display(Node** head) { list_display_range(head, NULL, NULL); }
 /// @param start_node first node to display
 /// @param end_node last node to display
 void list_display_range(Node** head, Node* start_node, Node* end_node) {
-    pthread_rwlock_rdlock(&lock);
+    pthread_rwlock_rdlock(&head_lock);
     if (end_node) end_node = end_node->next;
     if (!start_node) start_node = *head;
     printf("[");
@@ -163,21 +192,21 @@ void list_display_range(Node** head, Node* start_node, Node* end_node) {
         if (start_node && start_node != end_node) printf(", ");
     }
     printf("]");
-    pthread_rwlock_unlock(&lock);
+    pthread_rwlock_unlock(&head_lock);
 }
 
 /// @brief returns the number of nodes
 /// @param head list head
 /// @return int
 int list_count_nodes(Node** head) {
-    pthread_rwlock_rdlock(&lock);
+    pthread_rwlock_rdlock(&head_lock);
     Node* walker = *head;
     int counter = 0;
     while (walker != NULL) {
         counter++;
         walker = walker->next;
     }
-    pthread_rwlock_unlock(&lock);
+    pthread_rwlock_unlock(&head_lock);
     return counter;
 }
 
@@ -186,5 +215,5 @@ int list_count_nodes(Node** head) {
 void list_cleanup(Node** head) {
     *head = NULL;
     mem_deinit();
-    pthread_rwlock_destroy(&lock);
+    pthread_rwlock_destroy(&head_lock);
 }
